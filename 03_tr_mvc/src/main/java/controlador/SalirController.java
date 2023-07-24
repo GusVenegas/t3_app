ac36f6d62bc8d105658e145f80584c59b975cefe
Diana-PC <carolinabravo946@gmail.com>
2023-07-24 00:46:37 -0500

    boton salir

--- 03_tr_mvc/src/main/java/controlador/SalirController.java
+++ 03_tr_mvc/src/main/java/controlador/SalirController.java
@@ -23,7 +23,7 @@ public class SalirController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getSession().invalidate();
-		response.sendRedirect("LoginController");
+		response.sendRedirect("jsp/login.jsp");
 		
 	}
 
